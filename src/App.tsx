470e137cb24c249bde6e8a589a86f1df8cabf947
Soe Phyu <soephyuphyuhtun99@gmail.com>
2026-04-24 18:31:30 +0630

    - remove hallo

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import "./index.css";
 function App() {
   return (
     <div className="py-10 space-y-10 mx-4 lg:mx-10">
-      <h1 className="text-center text-3xl font-semibold">
-        React DND, halo, hello
-      </h1>
+      <h1 className="text-center text-3xl font-semibold">React DND, halo</h1>
       <Board />
     </div>
   );
